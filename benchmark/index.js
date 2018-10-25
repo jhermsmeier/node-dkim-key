a2e209e4b8fb562ebfc8a166f24c222f63e022a9
Jonas Hermsmeier <jhermsmeier@gmail.com>
2018-10-25 17:03:53 +0200

    test: Add benchmarks

---
+++ benchmark/index.js
@@ -0,0 +1,30 @@
+var bench = require( 'nanobench' )
+var DKIMKey = require( '..' )
+
+const ITERATIONS = 1000000
+
+bench( `⨉ ${ITERATIONS}`, function( run ) {
+
+  var record = 'k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1Kd87/UeJjenpabgbFwh+eBCsSTrqmwIYYvywlbhbqoo2DymndFkbjOVIPIldNs/m40KF+yzMn1skyoxcTUGCQs8g3FgD2Ap3ZB5DekAo5wMmk4wimDO+U8QzI3SD07y2+07wlNWwIt8svnxgdxGkVbbhzY8i+RQ9DpSVpPbF7ykQxtKXkv/ahW3KjViiAH+ghvvIhkx4xYSIc9oSwVmAl5OctMEeWUwg8Istjqz8BZeTWbf41fbNhte7Y+YqZOwq1Sd0DbvYAD9NOZK9vlfuac0598HY+vtSBczUiKERHv1yRbcaQtZFh5wtiRrN04BLUTD21MycBX5jYchHjPY/wIDAQAB'
+  var key = null
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    key = DKIMKey.parse( record )
+  }
+  run.end()
+
+})
+
+bench( `⨉ ${ITERATIONS}`, function( run ) {
+
+  var record = 'k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1Kd87/UeJjenpabgbFwh+eBCsSTrqmwIYYvywlbhbqoo2DymndFkbjOVIPIldNs/m40KF+yzMn1skyoxcTUGCQs8g3FgD2Ap3ZB5DekAo5wMmk4wimDO+U8QzI3SD07y2+07wlNWwIt8svnxgdxGkVbbhzY8i+RQ9DpSVpPbF7ykQxtKXkv/ahW3KjViiAH+ghvvIhkx4xYSIc9oSwVmAl5OctMEeWUwg8Istjqz8BZeTWbf41fbNhte7Y+YqZOwq1Sd0DbvYAD9NOZK9vlfuac0598HY+vtSBczUiKERHv1yRbcaQtZFh5wtiRrN04BLUTD21MycBX5jYchHjPY/wIDAQAB'
+  var key = DKIMKey.parse( record )
+
+  run.start()
+  for( var i = 0; i < ITERATIONS; i++ ) {
+    record = key.toString()
+  }
+  run.end()
+
+})
