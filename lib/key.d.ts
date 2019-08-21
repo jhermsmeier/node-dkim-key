2c8f9cce9f5e07d19a77ddccb9793ed73863f994
SmallhillCZ <github@smallhill.cz>
2019-08-21 14:09:48 +0200

    Add type definitions

---
+++ lib/key.d.ts
@@ -0,0 +1,47 @@
+interface KeyOptions {
+  version: string;
+  type: string;
+  flags: string;
+  granularity: string;
+  hash: string;
+  notes: string;
+  service: string;
+  key: string;
+}
+
+declare class Key {
+
+  version: string;
+  type: string;
+  flags: string;
+  granularity: string;
+  hash: string;
+  notes: string;
+  service: string;
+  key: string;
+
+  constructor(options: Partial<KeyOptions>);
+
+  static create(options: KeyOptions): Key;
+  static parse(value: string): any;
+
+  static fieldMap: {
+    g: string;
+    h: string;
+    k: string;
+    n: string;
+    p: string;
+    s: string;
+    t: string;
+    v: string;
+  };
+
+  static keys: string[];
+
+  parse(input: string): Key;
+
+  toString(): string;
+
+}
+
+export = Key;
